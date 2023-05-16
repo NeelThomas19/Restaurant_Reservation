3258dd69bc157e2f698dfa758b45ecbee2f76de6
Neel Thomas <20CS095@charusat.edu.in>
2023-05-16 23:46:41 +0530

    navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { GiHamburgerMenu } from "react-icons/gi";
 
-function Nav() {
+const Nav = () => {
   return (
-    <nav className='navbar'>
-      <ul className='nav-link'>
-        <li><a href='#'>Home</a></li>
-        <li><a href='#'>Menu</a></li>
-        <li><a href='#'>Contact</a></li>
-      </ul>
-    </nav>
-  );
+    <>
+      <nav className='navbar'>
+          <div className='logo'>
+            <img src='#' alt='logo'></img>
+          </div>
+          <div className='menu'>
+            <ul className='nav-link'>
+              <li><a href='#'>Home</a></li>
+              <li><a href='#'>Contact</a></li>
+            </ul>
+            <div className='hamburger'>
+                <a href='#'>
+                  <GiHamburgerMenu />
+                </a>
+            </div>
+          </div>
+      </nav>
+    </>
+  )
 }
 
-export default Nav;
+export default Nav
